e7482a0004dc299c1724da7b50173b0312f71ea1
tariktunahanakan <akantariktunahan@gmail.com>
2022-10-12 00:34:27 +0300

    update

--- hello.py
+++ hello.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Docker'
+    return 'Hello World'
 
-@app.route('/hello')
+@app.route('/docker')
 def hello():
-    return 'Hello, World'
+    return 'Hello Docker'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
